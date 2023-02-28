0a15e729f2400c5704b29442c16cb054c2f9d3a7
Akinola <akinolatofunmi04@gmail.com>
2023-02-28 11:24:04 +0100

    firebase installation

--- web_static/web_app/src/main.js
+++ web_static/web_app/src/main.js
@@ -1,4 +1,5 @@
 import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router/index'
