7e02630758378b7b1a4848cf44bb7a265eda0ab4
Isquare007 <akibrahiim2019@gmail.com>
2023-03-02 01:26:31 +0100

    signup route

--- vue/clibsync/src/views/SignupForm.vue
+++ vue/clibsync/src/views/SignupForm.vue
@@ -77,7 +77,7 @@ export default {
                 var user_id = userCredential.user.uid;
                 console.log(user_id);
                 localStorage.setItem('user_data', JSON.stringify(userData)); // Keeps userSigned by registering userData on localStorag
-                this.$router.push({ name: 'HomePage' }) //Takes signed up user to the home/dash
+                this.$router.push({ name: 'texts' }) //Takes signed up user to the home/dash
             }
             catch (error) {
                 console.log(error)
